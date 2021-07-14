34b002c8c92deed55a30dfcf8eea8a67ac8fc06d
Nodirbek Nazarov <uztiger1998@gmail.com>
2021-07-14 11:45:36 +0500

    basic code

---
+++ reactProject/my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './Home';
+import Pricing from './Pricing';
+import About from './About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Pricing/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
